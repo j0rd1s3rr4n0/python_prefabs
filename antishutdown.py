faebb62070737095794b7d1453a1b07ecaf65979
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2019-02-25 00:10:20 +0100

    Update antishutdown.py

--- antishutdown.py
+++ antishutdown.py
@@ -1,17 +1,13 @@
 import os
 import time
-if opsi == "windows" or "linux":
-	second = 0
-	minute = 0
-	hour = 0
-	start = True
+second = 0
+minute = 0
+hour = 0
+start = True
 while start != False:	
 	second = second + 1
 	print(hour,":",minute,":",second)
 	time.sleep(1)
-	if second == 5:
-		"""os.system('shutdowm -a')
-		os.system('cls')"""	
-		#If you use windows
-		"""os.system('shutdowm -a')
-		os.system('cls')"""
+	if second == 0.5:
+		os.system('shutdowm -a')
+		os.system('cls')
