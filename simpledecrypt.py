86c0c7b091a64a00017b9c0f7d5c839d2e864cf6
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2020-12-06 16:58:07 +0100

    Create simpledecrypt.py

---
+++ simpledecrypt.py
@@ -0,0 +1,17 @@
+count = -1
+filename = "save.txt"
+elements = ['/Ä27','/Ä01','/Ä02','/Ä03','/Ä04','/Ä05','/Ä06','/Ä07','/Ä08','/Ä09','/Ä10','/Ä11','/Ä12','/Ä13','/Ä14','/Ä15','/Ä16','/Ä17','/Ä18','/Ä29','/Ä20','/Ä21','/Ä22','/Ä23','/Ä24','/Ä25','/Ä26','/Ä28']
+replacin = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\n']
+for element in elements:
+	char1 = elements[count]
+	char2 = replacin[count]
+	filer = open(filename, "r")
+	filew = open(filename, "r+")	
+	buff = filer.read()
+	rbuff = buff.replace(char1, char2)
+	filew.write(rbuff)
+	filer.close()
+	filew.close()
+	#print(elements[count]+" replace to "+replacin[count])
+	count+=1
+
